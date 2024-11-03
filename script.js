78cadda5deb8fb8bdf14849eba70991b120c0085
Alexander Sviridov <sviridovalexander81@gmail.com>
2024-11-03 17:42:26 +0100

    Added variables Lesson02

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-alert('Hello Academy');
-
-const title = 1;
-const screens = 2;
-const screenPrice = 3;
+const title = 'Lesson02';
+const screens = 'Простые, Сложные, Интерактивные';
+const screenPrice = 30;
 const rollback = 4;
-const fullPrice = 5;
-const adaptive = 6;
+const fullPrice = 100;
+const adaptive = true;
 
-console.log(title);
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+console.log('Стоимость верстки экранов ' + screenPrice + ' долларов');
+console.log('Стоимость разработки сайта ' + fullPrice + ' рублей');
+
+console.log(screens.toLowerCase());
+console.log(screens.split(', '));
+
+console.log((fullPrice * rollback) / 100);
